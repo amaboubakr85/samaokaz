69f8b7c99ad912bd3a4671cab720c41ff383d79d
amaboubakr85 <a.m.aboubakr85@gmail.com>
2021-04-29 06:22:56 +0300

    planning page construct

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,43 @@
-document.querySelector('.nxt-sec').addEventListener('click', function () {
-  window.location.href = '#aboutus'
-})
+$next_sec = document.querySelector('.nxt-sec')
+if ($next_sec) {
+  $next_sec.addEventListener('click', function () {
+    window.location.href = '#aboutus'
+  })
+}
+
+let play = document.getElementById('play')
+if (play) {
+  play.addEventListener('click', function () {
+    let play_video = document.querySelector('.video-play')
+    document.querySelector('.overlay').style.opacity = '0'
+    document.querySelector('.overlay').style.zIndex = '0'
+    play_video.play()
+    play_video.controls = true
+  })
+}
+
+const planningGallery = document.querySelector('.planning-gallery')
+
+if (planningGallery) {
+  var galleryThumbs = new Swiper('.gallery-thumbs', {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    loop: true,
+    freeMode: true,
+    loopedSlides: 5, //looped slides should be the same
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+  })
+  var galleryTop = new Swiper('.gallery-top', {
+    spaceBetween: 10,
+    loop: true,
+    loopedSlides: 5, //looped slides should be the same
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+      swiper: galleryThumbs,
+    },
+  })
+}
